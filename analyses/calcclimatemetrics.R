6aee7e5f42a7b6de85f6529fdcc51f7670a3d7ba
lizzieinvancouver <lizzie@biodiversity.ubc.ca>
2023-03-22 11:03:05 +0100

    Finished the code for calculting mean and sd and deleting some unneccessary climate files
    Those files all still exist in the data folder.

--- analyses/calcclimatemetrics.R
+++ analyses/calcclimatemetrics.R
@@ -1,5 +1,8 @@
 ## Started 21 mars 2023 ##
+## By Lizzie ##
+
 ## Whoo-hoo, spring! Time to finally get some analyses going on Phenofit project ##
+## This codes reads in historical climate data and calculates mean and SD by month ##
 
 ## housekeeping
 rm(list=ls()) 
@@ -12,16 +15,17 @@ library(ggplot2)
 ## set working directory
 setwd("~/Documents/git/projects/treegarden/misc/climatehazards/analyses")
 
-## To do! Look for ## START HERE !!!! below...
-# Some quick calculations to check my code
+## To do!
+##
 
-justone <- fread("input/ERA5LAND/ERA5LAND_glo_1970_dly.fit")
+justone <- fread("input/ERA5LAND/ERA5LAND_tmn_1970_dly.fit")
 
-yrz <- c(1970:2000)
-filelist <-paste("input/ERA5LAND/ERA5LAND_tmn_", yrz, "_dly.fit", sep="")
+## Read in files and calculating mean and SD:
+# First function does it for tmin and tmax files
+# Second funtion calculates the mean of those files and then does it.
 
-resultzmin <- data.frame()
-
+fromfilestomeansd <- function(filelist, yearlist){  
+resultz <- data.frame()
 # loop through each file in the list
 for (i in c(1:length(filelist))) {
   # read in the data file
@@ -35,20 +39,71 @@ for (i in c(1:length(filelist))) {
       datasm <- data[,-c(1:2)]
      # loop through each month
       for (month in 1:12) {
-        # calculate the mean and variance for the month (doing some gymnastics to make chatGPT's idea work)
-        monthsdays  <- which(format(as.Date(paste0(yrz[i], "-01-01"), "%Y-%m-%d") + (0:364), "%m") == sprintf("%02d", month))
+        # calculate the mean and variance for the month doing some gymnastics to make chatGPT's idea work)
+        monthsdays  <- which(format(as.Date(paste0(yearlist[i], "-01-01"), "%Y-%m-%d") + (0:364), "%m") == sprintf("%02d", month))
         monthstartday <- monthsdays[1]
         monthendday <- monthsdays[length(monthsdays)]
         monthdata <- datasm[j, monthstartday:monthendday]
         meanhere <- mean(monthdata, na.rm=TRUE)
         sdhere <- sd(monthdata, na.rm=TRUE)
       # add the results to the data frame
-      resultzmin <- rbind(resultzmin, data.frame(lat=lat, lon=lon, month=month, year=yrz[i], mean=meanhere, sd=sdhere))
+      resultz <- rbind(resultz, data.frame(lat=lat, lon=lon, month=month, year=yearlist[i], mean=meanhere, sd=sdhere))
      }
    }
 }
+return(resultz)
+}
 
-resultzmin$counter <- rep(seq(1:(12*length(yrz))), nrow(justone))
+fromfilestomeansdtmean <- function(filelistmin, filelistmax, yearlist){
+resultz <- data.frame()
+# loop through each file in the list
+for (i in c(1:length(filelistmin))) {
+  # read in the data file
+    datamin <- fread(filelistmin[i])
+    datamin <- as.matrix(datamin)
+    datamax <- fread(filelistmax[i])
+    datamax <- as.matrix(datamax)
+  # calculate the mean and variance by month for each site
+  for (j in 1:nrow(datamin)) {
+    # get the latitude and longitude for the site
+      lat <- datamin[j,1]
+      lon <- datamin[j,2]
+      latmax <-  datamax[j,1]
+      if(identical(lat, latmax)==FALSE) {print("Ahh! Latitudes not the same across files!")}
+      dataminsm <- datamin[,-c(1:2)]
+      datamaxsm <- datamax[,-c(1:2)]
+      # now get the mean ...
+      datamean <- (dataminsm+datamaxsm)/2
+     # loop through each month
+      for (month in 1:12) {
+        # calculate the mean and variance for the month doing some gymnastics to make chatGPT's idea work)
+        monthsdays  <- which(format(as.Date(paste0(yearlist[i], "-01-01"), "%Y-%m-%d") + (0:364), "%m") == sprintf("%02d", month))
+        monthstartday <- monthsdays[1]
+        monthendday <- monthsdays[length(monthsdays)]
+        monthdata <- datamean[j, monthstartday:monthendday]
+        meanhere <- mean(monthdata, na.rm=TRUE)
+        sdhere <- sd(monthdata, na.rm=TRUE)
+      # add the results to the data frame
+      resultz <- rbind(resultz, data.frame(lat=lat, lon=lon, month=month, year=yearlist[i], mean=meanhere, sd=sdhere))
+     }
+   }
+}
+return(resultz)
+}
+
+
+resultzmin <- fromfilestomeansd(paste("input/ERA5LAND/ERA5LAND_tmn_", c(1970:2000), "_dly.fit", sep=""), c(1970:2000))
+resultzmax <- fromfilestomeansd(paste("input/ERA5LAND/ERA5LAND_tmx_", c(1970:2000), "_dly.fit", sep=""), c(1970:2000))
+resultztmean <- fromfilestomeansdtmean(paste("input/ERA5LAND/ERA5LAND_tmn_", c(1970:2000), "_dly.fit", sep=""), paste("input/ERA5LAND/ERA5LAND_tmx_", c(1970:2000), "_dly.fit", sep=""), c(1970:2000))
+
+
+
+
+##############
+## Plotting ##
+##############
+
+resultzmin$counter <- rep(seq(1:(12*length(c(1970:2000)))), nrow(justone))
 
 ggplot(resultzmin, aes(x=counter, y=mean, color=lat)) +
     geom_line() 
@@ -63,64 +118,36 @@ ggplot(resultzmin, aes(x=year, y=sd, group=lat, color=lat)) +
     # geom_smooth(method=lm) +
     facet_wrap(month~.)
 
-
-## Test my code above ...
-## START HERE !!!!
-library(tidyr)
-colnameshere <- c("lat", "lon", as.character(1:365))
-justone <- fread("input/ERA5LAND/ERA5LAND_glo_1970_dly.fit")
-names(justone) <- colnameshere
-
-goober  <- gather(justone, day, value,
-           -lat, -lon)
+ggplot(resultztmean, aes(x=year, y=mean, group=lat, color=lat)) +
+    geom_line() +
+    # geom_smooth(method=lm) +
+    facet_wrap(month~.)
 
 
 
-## ChatGPT output to get started with ...
 
-# Okay, assume I have read in the files. There is one file per year and in the files, the first two columns are latitude/longitude while each column is a day of year and each row is a different site. Can you please write code that would calculate the mean and variance by calendar month for each site and each year?
-
-# This code assumes that the files are all in the same directory, and that each file is named with the year in the first four characters (e.g., "2022_data.csv").
-
-# The main changes to the code are [my third attempt]:
-
-# We use as.Date() to convert the day of the year to a date object, which allows us to extract the month more easily.
-# We use which() and format() to select the columns that correspond to the current month, even when dealing with leap years.
-# We use sprintf() to format the month number with leading zeros, to match the format used in the date strings.
-# With these changes, the code should correctly handle leap years and produce accurate results for each calendar month.
-
-# set the working directory to where the files are located
-setwd("path/to/files")
-
-# get a list of all the data files in the directory
-file_list <- list.files()
-
-# create an empty data frame to store the results
-results <- data.frame()
-
-# loop through each file in the list
-for (file_name in file_list) {
-  # read in the data file
-  data <- read.csv(file_name, header=TRUE)
-
-  # calculate the mean and variance by month for each site
-  for (i in 1:nrow(data)) {
-    # get the latitude and longitude for the site
-    lat <- data[i,1]
-    lon <- data[i,2]
-
-    # loop through each month
-    for (month in 1:12) {
-      # calculate the mean and variance for the month
-      month_data <- data[i, which(format(as.Date(paste0(substr(file_name, 1, 4), "-01-01"), "%Y-%m-%d") + (0:364), "%m") == sprintf("%02d", month))]
-      mean_val <- mean(month_data, na.rm=TRUE)
-      var_val <- var(month_data, na.rm=TRUE)
-
-      # add the results to the data frame
-      results <- rbind(results, data.frame(Lat=lat, Lon=lon, Month=month, Year=substr(file_name, 1, 4), Mean=mean_val, Variance=var_val))
-    }
-  }
-}
 
 # save the results to a CSV file
-write.csv(results, "results.csv", row.names=FALSE)
+# write.csv(results, "results.csv", row.names=FALSE)
+
+
+if(FALSE){ ## Test my code above ...
+library(tidyr)
+colnameshere <- c("lat", "lon", as.character(1:365))
+names(justone) <- colnameshere
+
+testing  <- gather(justone, day, value,
+           -lat, -lon)
+
+# now I need month
+# so I use DOY to get a sequence of dates
+nhemi <- c(1:365)
+nhemidate <- as.Date(paste(nhemi, 1970, sep="-"), format="%j-%Y") # I checked: non-leap year
+doylookup <- data.frame(date=nhemidate, month=format(nhemidate, "%b"), doy=as.character(nhemi))
+testmerge <- merge(doylookup, testing, by.x="doy", by.y="day")
+meantest <- aggregate(testmerge["value"], testmerge[c("lat", "lon", "month")], FUN=mean)
+
+subset(resultzmin, year=="1970" & month=="4")
+subset(meantest, month=="Apr")
+## Good news, it works. 
+}
